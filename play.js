f07eb2249f69365064be35c767852c41f0cae67e
Faiem-qy <f.quanyoung@gmail.com>
2023-08-10 22:12:07 -0400

    made two functions, one to setup listener and one to listen for specific keys and display a message when the key is pressed

--- play.js
+++ play.js
@@ -1,7 +1,21 @@
 const connect = require('./client');
 
+//setup interface to handle user input from stdin
 
-// establish a connection with the game server
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", (key) => handleUserInput (key));// event listener 
+  return stdin;
+};
 
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log('exiting...');
+    process.exit();
+  }
+};
 
-
+setupInput()
